11e4f7f099706ce3ff913af61c7d53e73afb0122
budougumi0617 <budougumi0617@gmail.com>
2015-12-28 21:10:50 +0900

    Add fragments

--- SimpleApplication/app/src/main/java/com/budougumi0617/simpleapplication/MainActivity.java
+++ SimpleApplication/app/src/main/java/com/budougumi0617/simpleapplication/MainActivity.java
@@ -1,13 +1,18 @@
 package com.budougumi0617.simpleapplication;
 
+
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,14 +21,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        final TextView textView = (TextView) findViewById(R.id.greet_view);
+        Button button = (Button) findViewById(R.id.button);
+        Button fragment_button = (Button) findViewById(R.id.manage_fragment);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+        if(fragment == null) {
+            fragment = new MyFragment1();
+            FragmentTransaction fragmentTransaction =
+                    getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.add(R.id.fragment_container, fragment).commit();
+        }
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                int resId = textView.getText().equals(getString(R.string.good_evening))?
+                        R.string.hello_world : R.string.good_evening;
+                textView.setText(resId);
+            }
+        });
+        fragment_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+                fragment = fragment.getClass() == MyFragment1.class ? new MyFragment2() : new MyFragment1();
+                FragmentTransaction fragmentTransaction =
+                        getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, fragment).commit();
             }
         });
     }
